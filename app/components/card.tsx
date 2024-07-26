e0a16929b7bc133ad59b19219ca9a55fdd8e0077
symensjeffrey <jeffrey.symens23@gmail.com>
2024-07-25 21:47:58 -0500

    change card background

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -7,10 +7,10 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ name, image_url }) => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card card-compact bg-neutral w-96 shadow-xl">
       <figure><img src={image_url} alt={name} /></figure>
       <div className="card-body">
-        <h2 className="card-title">{name}</h2>
+        <h2 className="card-title justify-center">{name}</h2>
       </div>
     </div>
   );
