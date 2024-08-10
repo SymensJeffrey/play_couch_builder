c475876477c03dcc1823d538b607dc06ce33478e
symensjeffrey <jeffrey.symens23@gmail.com>
2024-08-09 22:00:57 -0500

    add back href to nav bar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,7 +3,7 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Play Couch Builder</a>
+        <a href="/" className="btn btn-ghost text-xl">Play Couch Builder</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
