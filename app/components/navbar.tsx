f31fd32124e65e8dd509e5f8d5372d7098d14dbc
symensjeffrey <jeffrey.symens23@gmail.com>
2024-08-09 21:52:39 -0500

    update nav bar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,22 +3,20 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a href="/" className="btn btn-ghost text-xl">Play Couch Builder</a>
+        <a className="btn btn-ghost text-xl">Play Couch Builder</a>
       </div>
       <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block h-5 w-5 stroke-current">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
-          </svg>
-        </button>
+        <ul className="menu menu-horizontal px-1">
+          <li className="w-40">
+            <details>
+              <summary className="text-right">Builds</summary>
+              <ul className="bg-base-100 rounded-t-none p-2">
+                <li><a>All Builds</a></li>
+                <li><a>Submit a Build</a></li>
+              </ul>
+            </details>
+          </li>
+        </ul>
       </div>
     </div>
   );
