c6bc3b7f91bbd2f715e91469b9a8dcb653636058
symensjeffrey <jeffrey.symens23@gmail.com>
2024-08-21 21:16:48 -0500

    add mail function

--- app/buildCreate/page.tsx
+++ app/buildCreate/page.tsx
@@ -2,17 +2,33 @@
 import SectionTitle from '../components/Common/SectionTitle';
 
 const BuildSubmissionPage = () => {
-
+  const handleSendEmail = () => {
+    window.location.href = 'mailto:jsdevlabs@gmail.com';
+  }
   return (
     <section className="relative z-10 py-16 md:py-20 lg:py-28 top-20">
       <div className="container">
         <SectionTitle
           title="This page is not ready yet"
-          paragraph="If you would like to submit a build to be featured on here please email an image and title of the build to: "
+          paragraph={
+            <>
+              If you would like to submit a build to be featured on here, please email an image and title of the build to:{" "}
+              <a href="mailto:jsdevlabs@gmail.com" style={{ color: 'inherit', textDecoration: 'underline' }}>
+                jsdevlabs@gmail.com
+              </a>
+            </>
+          }
           center
           mb="80px"
         />
-
+        <div className="flex items-center justify-center">
+          <button
+            className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+            onClick={handleSendEmail}
+          >
+            Send Email
+          </button>
+        </div>
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
             <div
